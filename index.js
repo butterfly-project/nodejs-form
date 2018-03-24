e705d2075595927e3167a3060a2f9b3b903b6ab4
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2018-03-24 20:27:34 +0700

    # Added transformers # Added scalar constraint # Added array constraint

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const validator = require('./validator');
+const transformer = require('./transformer');
+const scalarConstraint = require('./scalar-constraint');
+const arrayConstraint = require('./array-constraint');
 
 module.exports = {
     validator,
+    transformer,
+    scalarConstraint,
+    arrayConstraint,
 };
